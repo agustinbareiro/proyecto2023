6aedc64031f0b2a2a73a92690c180e2fff59bf87
Daniel Frías <friasdaniel996@gmail.com>
2023-06-22 15:57:31 -0300

    clase 22/6

---
+++ blog/apps/noticias/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class Noticia(models.Model):
+#     titulo = models.CharField(max_length=150)
+#     categoria_notica = models.ForeignKey(Categoria, on_delete=models.CASCADE)
